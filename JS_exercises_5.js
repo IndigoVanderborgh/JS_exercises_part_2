ff1b26b73e691c72cff8adbf2197804c26095274
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-12 18:25:11 -0700

    Update JS_exercises_5.js

--- JS_exercises_5.js
+++ JS_exercises_5.js
@@ -4,6 +4,15 @@ let weekday = "Friday"
 // If it is Friday the 13th, log out this spooky face: 😱
 // Use the logical "AND operator" -> &&
 
+// Don't forget the "OR operator" -> ||
+
 if (dayOfMonth === 31 && weekday === "Friday") {
     console.log("😱")
 }
+
+// Below is a a use of the OR operator -> ||
+// } else (dayOfMonth === 31 || weekday === "Friday") {
+//     console.log("Close 😱")
+// }
+
+// This exercise is to demonstrate knowledge on the AND/OR operator.
