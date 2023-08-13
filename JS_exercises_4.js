3e777b162616884b82a3811bbd222b221b2b8e0f
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-12 17:31:24 -0700

    Update JS_exercises_4.js

--- JS_exercises_4.js
+++ JS_exercises_4.js
@@ -1,15 +1,21 @@
 let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
 
 // You need to help me fixup the largeCountries array so that 
-// China and Pakistan are added back into their respective places
+// China and Pakistan are added back into their respective places (remove Tuvala and Monaco)
 
 // Use push() & pop() and their counterparts unshift() & shift()
 // Google how to use unshift() and shift()
 
-largeCountries.pop() // 
-largeCountries.push("Pakistan") // 
-largeCountries.shift() // 
-largeCountries.unshift("China") // 
-console.log(largeCountries) // 
+// Basics of Array Methods
+// pop--> remove elements to the end of the array
+// push --> add elements to the end of the array
+// shift --> remove elemens from the front of the array
+// unshift --> add elements to the front of the array
+
+largeCountries.pop() // removes last element from largeCountries array
+largeCountries.push("Pakistan") // adds last element "Pakistan" to largeCountries array
+largeCountries.shift() // removes first element from largeCountries array
+largeCountries.unshift("China") // adds first element "China" to largeCountries array
+console.log(largeCountries) // console log array
 
 // This exercise is to demonstrate knowledge on arrays, specifially pop and shift.
