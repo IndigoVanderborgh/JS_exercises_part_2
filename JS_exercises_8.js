aa148b068496780a0265341bb883befb12fad7b7
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-20 19:24:56 -0700

    Update JS_exercises_8.js

--- JS_exercises_8.js
+++ JS_exercises_8.js
@@ -1,11 +1,12 @@
 // template strings/literals
 
 const recipient = "James"
+const sender = "Indi"
 
 // Refactor the email string to use template strings
 // const email = "Hey " + recipient + "! How is it going? Cheers Per"
 
-const email = `Hey ${recipient}! How is it going? Cheers Per`
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
 
 // Notice how we concactinated the sentence to fit the html format, but with Js elements
 // we used template strings; the backticks.
